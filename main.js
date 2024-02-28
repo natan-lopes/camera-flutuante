316b321fd9debea3f120a7b218dfab742bc26121
natan-lopes <nemevioo@gmail.com>
2024-02-28 15:50:24 -0300

    camera

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    transparent:true,
+    alwaysOnTop: true,
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadFile('index.html')
+  win.setAlwaysOnTop(true, 'screen')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
